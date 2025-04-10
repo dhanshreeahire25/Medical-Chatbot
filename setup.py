edb479cc8e7edf536c4065153f5a07235f4ec07e
dhanshreeahire25 <dhanshreeahire25@gmail.com>
2025-04-10 20:19:51 +0530

    updated this request

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Generative AI Project',
     version='0.0.0',
     author='Dhanshree Ahire',
-    author_email='dhanshreeahire25@gmail.com',
+    author_email='sagarahire20025@gmail.com',
     packages= find_packages(),
     install_requires=[]
 )
